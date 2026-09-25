5124fb94cc0ab88978d633a7e28f45bc5e8e0aaf
akanfields1 <aka.nfields1@gmail.com>
2026-09-24 21:44:50 -0500

    Update version to 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.eleven"
         minSdk = 26
         targetSdk = 33
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     buildTypes {
